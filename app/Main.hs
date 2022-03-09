2814b13facf98438e03b8021485f725dc05f7c92
rhl120 <rhl120@protonmail.com>
2022-03-09 22:33:54 +0100

    Refactor movement

--- app/Main.hs
+++ app/Main.hs
@@ -72,72 +72,57 @@ isFree b color s@(x, y)
   where
     piece = b !! y !! x
 
-kinghtPossibilties :: Board -> Square -> Either String [Square]
-kinghtPossibilties b s@(x, y)
+knightPossibs :: Board -> Square -> [Square]
+knightPossibs b s@(x, y) =
+  [ (x + 1, y + 2)
+  , (x - 1, y + 2)
+  , (x - 2, y + 1)
+  , (x - 2, y - 1)
+  , (x - 1, y - 2)
+  , (x + 1, y - 2)
+  , (x + 2, y - 1)
+  , (x + 2, y + 1)
+  ]
+
+pawnPossibs :: Board -> PlayerColor -> Square -> [Square]
+pawnPossibs b c s@(x, y) =
+  if c == Black
+    then [(x, y + 1), (x + 1, y + 1), (x - 1, y + 1)] ++ [(x, y + 2) | y == 1]
+    else [(x, y - 1), (x + 1, y - 1), (x - 1, y - 1)] ++ [(x, y - 2) | y == 7]
+
+rookPossibs :: Board -> Square -> [Square]
+rookPossibs b s@(x, y) =
+  [(x + n, y) | n <- [-7 .. 7]] ++ [(x, y + n) | n <- [-7 .. 7]]
+
+bishopPossibs :: Board -> Square -> [Square]
+bishopPossibs b s@(x, y) =
+  [(x + n, y + n) | n <- [-7 .. 7]] ++ [(x - n, y + n) | n <- [-7 .. 7]]
+
+queenPossibs :: Board -> Square -> [Square]
+queenPossibs b s = rookPossibs b s ++ bishopPossibs b s
+
+getPossibs :: Board -> Square -> Either String [Square]
+getPossibs b s@(x, y)
   | not $ boardIsFit b = Left "Board is not an 8x8 matrix"
   | not $ isValidSquare s = Left "Square is not valid"
+  | piece == Empty = Right []
   | otherwise =
     Right $
-    filter (\z -> isFree b c z == Right True) (filter isValidSquare possibs)
+    filter
+      (\z -> isFree b (pieceColor piece) z == Right True)
+      (filter isValidSquare pt)
   where
-    possibs =
-      [ (x + 1, y + 2)
-      , (x - 1, y + 2)
-      , (x - 2, y + 1)
-      , (x - 2, y - 1)
-      , (x - 1, y - 2)
-      , (x + 1, y - 2)
-      , (x + 2, y - 1)
-      , (x + 2, y + 1)
-      ]
-    c = pieceColor (b !! y !! x)
-
-pawnPossibilities :: Board -> Square -> Either String [Square]
-pawnPossibilities b s@(x, y)
-  | not $ boardIsFit b = Left "Board is not an 8x8 matrix"
-  | not $ isValidSquare s = Left "Square is not valid"
-  | otherwise =
-    Right $
-    filter (\z -> isFree b c z == Right True) (filter isValidSquare possibs)
-  where
-    possibs =
-      if c == Black
-        then [(x, y + 1), (x + 1, y + 1), (x - 1, y + 1)] ++
-             [(x, y + 2) | y == 1]
-        else [(x, y - 1), (x + 1, y - 1), (x - 1, y - 1)] ++
-             [(x, y - 2) | y == 7]
-    c = pieceColor (b !! y !! x)
-
-rookPosibilities :: Board -> Square -> Either String [Square]
-rookPosibilities b s@(x, y)
-  | not $ boardIsFit b = Left "Board is not an 8x8 matrix"
-  | not $ isValidSquare s = Left "Square is not valid"
-  | otherwise =
-    Right $
-    filter (\z -> isFree b c z == Right True) (filter isValidSquare possibs)
-  where
-    possibs = [(x + n, y) | n <- [-7 .. 7]] ++ [(x, y + n) | n <- [-7 .. 7]]
-    c = pieceColor (b !! y !! x)
-
-bishopPossibs :: Board -> Square -> Either String [Square]
-bishopPossibs b s@(x, y)
-  | not $ boardIsFit b = Left "Board is not an 8x8 matrix"
-  | not $ isValidSquare s = Left "Square is not valid"
-  | otherwise =
-    Right $
-    filter (\z -> isFree b c z == Right True) (filter isValidSquare possibs)
-  where
-    possibs =
-      [(x + n, y + n) | n <- [-7 .. 7]] ++ [(x - n, y + n) | n <- [-7 .. 7]]
-    c = pieceColor (b !! y !! x)
-
-queenPossibs :: Board -> Square -> Either String [Square]
-queenPossibs b s = do
-  rook <- rookPosibilities b s
-  bishop <- bishopPossibs b s
-  return $ rook ++ bishop
+    piece = b !! y !! x
+    pt =
+      case pieceType piece of
+        Knight -> knightPossibs b s
+        Rook -> rookPossibs b s
+        Bishop -> bishopPossibs b s
+        Pawn -> pawnPossibs b (pieceColor piece) s
+        Queen -> queenPossibs b s
+        _ -> error "A piece has has no moves please report this as a bug"
 
 main :: IO ()
 main = do
   f <- readFile "./b"
-  print $ parseBoard f
+  return ()
